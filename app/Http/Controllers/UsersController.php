8ec1e934931e28bc87cc108b5a8b621a255e885c
Omar Tawba <omartawba1@users.noreply.github.com>
2021-05-26 08:30:28 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Http\Requests\UsersRequest;
 use App\Models\User;
 use App\Notifications\NewUserNotification;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
 
 class UsersController extends Controller
